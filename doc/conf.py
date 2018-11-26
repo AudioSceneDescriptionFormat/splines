af8a3c643731a781c710e6333944f3fa8d1abda1
Matthias Geier <Matthias.Geier@gmail.com>
2018-11-26 22:25:21 +0100

    add javascript for sphinx < 1.8

--- doc/conf.py
+++ doc/conf.py
@@ -16,10 +16,17 @@ extensions = [
 
 highlight_language = 'none'
 html_sourcelink_suffix = ''
-html_js_files = [
-    'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js',
-    DEFAULT_EMBED_REQUIREJS_URL,
-]
+
+# Sphinx >= 1.8:
+#html_js_files = [
+#    'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js',
+#    DEFAULT_EMBED_REQUIREJS_URL,
+#]
+
+# Sphinx < 1.8:
+def setup(app):
+    app.add_javascript('https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js')
+    app.add_javascript(DEFAULT_EMBED_REQUIREJS_URL)
 
 intersphinx_mapping = {'https://docs.python.org/': None}
 
