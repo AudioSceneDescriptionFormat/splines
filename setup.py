2e45c8cb63db4a55252e2119be4f0e7e1f926a11
Matthias Geier <Matthias.Geier@gmail.com>
2023-01-08 19:22:10 +0100

    Update build system to avoid warnings

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
+from pathlib import Path
+
 from setuptools import setup
 
 # "import" __version__
 __version__ = 'unknown'
-for line in open('src/splines/__init__.py'):
-    if line.startswith('__version__'):
-        exec(line)
-        break
+with Path('src/splines/__init__.py').open() as f:
+    for line in f:
+        if line.startswith('__version__'):
+            exec(line)
+            break
 
 setup(
     name='splines',
@@ -15,11 +18,12 @@ setup(
     author='Matthias Geier',
     author_email='Matthias.Geier@gmail.com',
     description='Splines in Euclidean Space and Beyond',
-    long_description=open('README.rst').read(),
+    long_description=Path('README.rst').read_text(),
     install_requires=['NumPy'],
     python_requires='>=3.7',
     license='MIT',
     keywords='splines curves interpolation quaternions'.split(),
+    url='https://splines.readthedocs.io/',
     project_urls={
         'Documentation': 'https://splines.readthedocs.io/',
         'Source Code': 'https://github.com/AudioSceneDescriptionFormat/splines/',
